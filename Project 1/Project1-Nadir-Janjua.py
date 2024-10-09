a2ede70fd5701bb2cfcae759f1b5f8753b7cd23a
nadir580 <nadir.janjua@torontomu.ca>
2024-10-09 10:54:34 -0400

    Updated Project 1 Step 1

--- Project 1/Project1-Nadir-Janjua.py
+++ Project 1/Project1-Nadir-Janjua.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Step 1
+df = pd.read_csv('Project_1_Data.csv')
+print(df.info())
 
-file_path = '/Users/nadir580/Documents/GitHub/AER850-/Project 1/Project_1_Data.csv'
-data = pd.read_csv('Project_1_Data.csv')
-
-print(data.info())
-
+#Step 2
