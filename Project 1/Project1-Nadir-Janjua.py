f18850d7948fba80c6bddd40ccfc19e13d16c873
nadir580 <nadir.janjua@torontomu.ca>
2024-10-09 10:52:21 -0400

    Step 1
    Added stepone

---
+++ Project 1/Project1-Nadir-Janjua.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+file_path = '/Users/nadir580/Documents/GitHub/AER850-/Project 1/Project_1_Data.csv'
+data = pd.read_csv('Project_1_Data.csv')
+
+print(data.info())
+
